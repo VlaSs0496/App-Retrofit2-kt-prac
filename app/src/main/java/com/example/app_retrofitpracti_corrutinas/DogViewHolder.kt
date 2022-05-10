43d6d0586108b9a3fd4f09f02f0c13ab1564195a
VlaSs0496 <vladimirfb18@outlook.es>
2022-05-09 19:48:08 -0500

    Complete Adapter

--- app/src/main/java/com/example/app_retrofitpracti_corrutinas/DogViewHolder.kt
+++ app/src/main/java/com/example/app_retrofitpracti_corrutinas/DogViewHolder.kt
@@ -3,9 +3,14 @@ package com.example.app_retrofitpracti_corrutinas
 import android.provider.MediaStore
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.app_retrofitpracti_corrutinas.databinding.ItemDogBinding
+import com.squareup.picasso.Picasso
 
 class DogViewHolder (view:View):RecyclerView.ViewHolder(view){
-    fun bind(images: String){
 
+    private val bindind = ItemDogBinding.bind(view)
+    fun bind(image: String){
+        Picasso.get().load(image).into(bindind.ivDog)
+        bindind.ivDog
     }
 }
